b1e390c02e1e89c9fcb0c038b2fc5ebed856b098
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-10 00:14:29 +0900

    ログイン追加

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <form action="login_out.php" method="post">
+        ログイン名<input typr="text" name="login"><br>
+        パスワード<input type="password" name="password"><br>
+        <input type="submit" value="ログイン">
+    </form>
+</body>
+</html>
---
+++ login_out.php
@@ -0,0 +1,20 @@
+<?php
+unset($_SESSION['Uzer']);
+$pdo=new PDO($connect,USER,PASS);
+$sql=$pdo->prepare('select * from Uzer where login=?');
+$sql->execute([$_POST['login']]);
+foreach ($sql as $row){
+    if(password_verify($_POST['password'],$row['password'])){
+    $_SESSION['Uzer']=[
+        'id' =>$row['uzer_id'],'name'=>$row['name'],
+        'login'=>$row['login'],
+        'password'=>$row['password']];
+    }
+    
+}
+if(isset($_SESSION['Uzer'])){
+    echo '<a href="top.php">'; //top.phpにとばす　なんだっけ？処理
+}else {
+    echo 'ログイン名またはパスワードが違います';
+}
+?>
