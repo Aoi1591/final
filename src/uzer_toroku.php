d5400d64314cefa3aeae5ab2a0c20c2bb60c8f24
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-12 14:20:14 +0900

    追加

--- src/uzer_toroku.php
+++ src/uzer_toroku.php
@@ -5,25 +5,29 @@
     <body>
 
     <center><h1>ユーザー登録</h1></center>
-    
-    <div id="app">
-        <form id="appForm" action="U_info-output.php" method="post">
-                    
-                    <h4>名前</h4>
-                    <input v-model="name" type="text" name="Name" class="text">
-                    <div v-if="isNameError" class="error">名前を入力してください</div>
-                    
-                    <h4>パスワード</h4>
-                    <input v-model="nickname" type="text" name="Nickname" class="text">
-                    <div v-if="isNicknameError" class="error">パスワードを入力してください</div>
+    <?php
+       $name=$address=$login=$password='';
 
-                    <h4>生年月日</h4>    
-                    <input type="date" size="30" name="birthday" class="text">
-                    <div v-if="isPhoneNumberError" class="error">生年月日を入力してください</div>
-
-                    <button class="example" type="button" @click="submitForm"><span>登録</span></button>
-        </form>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    <script src="../script/U_info.js"></script>
+       if(isset($_SESSION['Uzer'])){
+         $name=$_SESSION['Uzer']['name'];
+         $address=$_SESSION['Uzer']['address'];
+         $password=$_SESSION['Uzer']['password'];
+       }
+   
+       echo '<form action="customer-output.php" method="post">';
+       echo '<table>';
+       echo '<tr><td>お名前</td><td>';
+       echo '<input type="text" name="name" value="',$name,'">';
+       echo '</td></tr>';
+       echo '<tr><td>メールアドレス</td><td>';
+       echo '<input type="text" name="address" value="',$address,'">';
+       echo '</td></tr>';
+       echo '<tr><td>パスワード</td><td>';
+       echo '<input type="password" name="password" value="',$password,'">';
+       echo '</td></tr>';
+       echo '</table>';
+       echo '<input type="submit" value="確定">';
+       echo  '</form>';
+    ?>
 </body>
 </html>
---
+++ src/uzer_toroku_output.php
@@ -0,0 +1,36 @@
+<?php session_start();?>
+<?php require 'header.php';?>
+<?php require 'menu.php';?>
+<?php require 'db-connect.php';?>
+<?php
+$pdo = new PDO($connect,USER,PASS);
+if(isset($_SESSION['Uzer'])){
+    $id=$_SESSION['Uzer']['id'];
+    $sql=$pdo->prepare('select * from Uzer where id!=? and address=?');
+    $sql->execute([$id,$_POST['adress']]);
+}else{
+    $sql=$pdo->prepare('select * from Uzer where address=?');
+    $sql->execute([$_POST['address']]);
+}
+if(empty($sql->fetchAll())){
+    if(isset($_SESSION['Uzer'])){
+        $sql=$pdo->prepare('update Uzer set name=?,address=?,password=? where id=?');
+        
+        $sql->execute([
+            $_POST['name'],$_POST['address'],$_POST['password'],$id]);
+        $_SESSION['UZer']=[
+            'id'=>$id,'name'=>$_POST['name'],
+            'address'=>$_POST['address'],
+            'passwoed'=>$_POST['password']];
+        echo 'お客様情報を更新しました。';
+    }else{
+        $sql=$pdo->prepare('insert into Uzer values(null,?,?,?)');
+        $sql->execute([
+            $_POST['name'],$_POST['address'],$_POST['password']]);
+        echo 'お客様情報を登録しました。';
+    }
+}else{
+    echo 'ログイン名がすでに使用されていますので、変更してください。';
+}
+?>
+<?php require 'footer.php';?>
