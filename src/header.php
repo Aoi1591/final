e3625c066ee0a4b2ff4921fb9362380e5b374e1d
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-17 14:28:08 +0900

    header

--- src/header.php
+++ src/header.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ハンバーガーメニューのサンプル</title>
-  <link rel="stylesheet" href="header.css">
-  <script src="script.js" defer></script>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>タイトル</title>
 </head>
 <body>
-  <header>
-    <div class="hamburger-menu">
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-    </div>
-    <nav class="nav-menu">
-    <ul>
-        <li><a href="top.php">トップ</a></li>
-        <li><a href="toroku.php">新規登録</a></li>
-        <li><a href="logout.php">ログアウト</a></li>
-        <li><a href="my.php">マイページ</a></li>
-      </ul>
-    </nav>
-  </header>
+<a href="top.php">トップ</a>
+<a href="toroku.php">新規登録</a>
+<a href="logout-input.php">ログアウト</a>
+<a href="my.php">マイページ</a>
+<hr>
 </body>
 </html>
