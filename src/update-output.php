9276aec8b7ef64012e76f22125ddf95456fd32c5
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-17 12:27:36 +0900

    修正

--- src/update-output.php
+++ src/update-output.php
@@ -1,4 +1,5 @@
 <?php require 'db_connect.php';?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,9 +11,11 @@
     
 <?php
 $pdo = new PDO($connect, USER, PASS);
-$sql = $pdo->prepare('update Money set line_day=?, name=?, money=?, category_id=?, memo=? where id=?');
+
+// 更新処理
+$sqlUpdate = $pdo->prepare('update Money set line_day=?, name=?, money=?, category_id=?, memo=? where id=?');
           
-if(empty($_POST['line_day']) || empty($_POST['name']) || !preg_match('/[0-9]+/', $_POST['money'])){
+if (empty($_POST['line_day']) || empty($_POST['name']) || !preg_match('/[0-9]+/', $_POST['money'])) {
     echo '日付、用途、金額を正しく入力してください';
 } else {
     $line_day = htmlspecialchars($_POST['line_day']);
@@ -21,30 +24,25 @@ if(empty($_POST['line_day']) || empty($_POST['name']) || !preg_match('/[0-9]+/',
     $category_id = $_POST['category_id'];
     $memo = htmlspecialchars($_POST['memo']);
     
-    if($sql->execute([$line_day, $name, $money, $category_id, $memo, $_POST['id']])){
+    if ($sqlUpdate->execute([$line_day, $name, $money, $category_id, $memo, $_POST['id']])) {
         echo '更新に成功しました。';
     } else {
         echo '更新に失敗しました。';
     }
 }
-?>
-<hr>
-<table>
-    <tr><th>日付</th><th>用途</th><th>金額</th><th>カテゴリー</th><th>メモ</th></tr>
 
-<?php
-foreach($pdo->query('select * from Money') as $row){
-    echo '<tr>';
-    echo '<td>', $row['line_day'], '</td>';
-    echo '<td>', $row['name'], '</td>';
-    echo '<td>', $row['money'], '</td>';
-    echo '<td>', $row['category_id'], '</td>';
-    echo '<td>', $row['memo'], '</td>';
-    echo '</tr>';
-    echo "\n";
-}
-?>
-</table>
+// 挿入処理
+$sqlInsert = $pdo->prepare('insert into Money (line_day, name, money, category_id, memo) values (?, ?, ?, ?, ?)');
 
-</body>
-</html>
+if (isset($_POST['insert'])) {
+    // 新しいデータを挿入
+    $line_day = htmlspecialchars($_POST['line_day']);
+    $name = htmlspecialchars($_POST['name']);
+    $money = $_POST['money'];
+    $category_id = $_POST['category_id'];
+    $memo = htmlspecialchars($_POST['memo']);
+    
+    if ($sqlInsert->execute([$line_day, $name, $money, $category_id, $memo])) {
+        echo '挿入に成功しました。';
+    } else {
+        echo '挿入に失敗し
