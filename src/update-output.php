6edc5b8ccd4c28618652394090a9cfaa25438aa5
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-17 12:22:42 +0900

    変更

--- src/update-output.php
+++ src/update-output.php
@@ -9,41 +9,42 @@
 <body>
     
 <?php
-          $pdo=new PDO($connect,USER,PASS);
-          $sql=$pdo->prepare('update Money set line_day=?,name=?,money=?,category_id=?,memo=? where id=?');
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('update Money set line_day=?, name=?, money=?, category_id=?, memo=? where id=?');
           
-          if(empty($_POST['line_day'])){
-            echo '日付を入力してください';
-          }else if(empty($_POST['name'])){
-             echo '用途を入力してください。';
-          }else if(!preg_match('/[0+9]+/',$_POST['money'])){
-             echo '金額を整数で入力しください。';
-          
-           }else
-            if($sql->execute([htmlspecialchars($_POST['line_day'],$_POST['name']),$_POST['money'],$_POST['category_id'],$_POST['memo']])){
-            echo '更新に成功しました。';
-           }else  {
-            echo '更新に失敗しました。';
-           }
-        ?>
-        <hr>
-        <table>
-            <tr><th>日付</th><th>用途</th><th>金額</th><th>カテゴリー</th><th>メモ</th></tr>
-
-        <?php
-        foreach($pdo->query('select*from Money') as $row){
-            echo '<tr>';
-            echo '<td>',$row['line_day'],'</td>';
-            echo '<td>',$row['name'],'</td>';
-            echo '<td>',$row['money'],'</td>';
-            echo '<td>',$row['category_id'],'</td>';
-            echo '<td>',$row['memo'],'</td>';
-            echo '</tr>';
-            echo "\n";
-        }
-        ?>
-        </table>
-
+if(empty($_POST['line_day']) || empty($_POST['name']) || !preg_match('/[0-9]+/', $_POST['money'])){
+    echo '日付、用途、金額を正しく入力してください';
+} else {
+    $line_day = htmlspecialchars($_POST['line_day']);
+    $name = htmlspecialchars($_POST['name']);
+    $money = $_POST['money'];
+    $category_id = $_POST['category_id'];
+    $memo = htmlspecialchars($_POST['memo']);
     
+    if($sql->execute([$line_day, $name, $money, $category_id, $memo, $_POST['id']])){
+        echo '更新に成功しました。';
+    } else {
+        echo '更新に失敗しました。';
+    }
+}
+?>
+<hr>
+<table>
+    <tr><th>日付</th><th>用途</th><th>金額</th><th>カテゴリー</th><th>メモ</th></tr>
+
+<?php
+foreach($pdo->query('select * from Money') as $row){
+    echo '<tr>';
+    echo '<td>', $row['line_day'], '</td>';
+    echo '<td>', $row['name'], '</td>';
+    echo '<td>', $row['money'], '</td>';
+    echo '<td>', $row['category_id'], '</td>';
+    echo '<td>', $row['memo'], '</td>';
+    echo '</tr>';
+    echo "\n";
+}
+?>
+</table>
+
 </body>
 </html>
