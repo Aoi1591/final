9e7918dd07675bb74d23f3c59f9fdb4c3117c861
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-01-18 13:07:03 +0900

    追加

--- src/php/toroku.php
+++ src/php/toroku.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
+<?php require 'db_connect.php';?>
+<?php require 'title.php';?>
+<?php require 'header.php';?>
+<!--<!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登録</title>
 </head>
-<body>
+-->
     <h2>登録</h2>
     <?php
    $name=$address=$login=$password='';
